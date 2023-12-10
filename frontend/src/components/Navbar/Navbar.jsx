3cb7c023595f3177a54b84016d426eae427611c4
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-12-10 22:25:49 +0000

    Added home, explore, and bookmarks NavLinks on Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   // @todo: create authcontext and replace placeholder
@@ -7,14 +7,29 @@ function Navbar() {
   return (
     <nav className="navbar-container">
       <div className="navbar-inner">
-        <Link to="/">
-          <div className="site-logo">
+        <Link to="/" className="site-logo">
+          <div>
             <img src="/images/tweeter-logo.png" alt="tweeter logo" />
+
+            {/* todo: add tweeter to appear when reached tablet size */}
             <span>Tweeter</span>
           </div>
         </Link>
 
+        <div className="nav-tabs">
+          <NavLink to="/" className="nav-home">
+            Home
+          </NavLink>
+          <NavLink to="/explore" className="nav-explore">
+            Explore
+          </NavLink>
+          <NavLink to="/bookmarks" className="nav-bookmarks">
+            Bookmarks
+          </NavLink>
+        </div>
+
         {userInfo && (
+          // @todo: test with a logged in user and add styles
           <div className="user-info">
             <div className="avatar"></div>
             <div className="username"></div>
@@ -24,7 +39,7 @@ function Navbar() {
 
         {!userInfo && (
           <Link to="/login" className="nav-login">
-            <div className="">Login</div>
+            <div>Login</div>
           </Link>
         )}
       </div>
