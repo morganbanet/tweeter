10521ef60835e1a2d1b5f8bacf2857531305ffe2
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-12-12 14:59:47 +0000

    Created dropdown component menu items for navbar

--- frontend/src/components/Dropdown/Dropdown.jsx
+++ frontend/src/components/Dropdown/Dropdown.jsx
@@ -1,5 +1,41 @@
+import { NavLink } from 'react-router-dom';
+
 function Dropdown() {
-  return <div className="nav-dropdown"></div>;
+  return (
+    <div className="nav-dropdown">
+      <div className="menu-items">
+        <div>
+          <div>
+            <i className="fa-solid fa-circle-user" />
+          </div>
+          <NavLink to="/profile">My Profile</NavLink>
+        </div>
+
+        <div>
+          <div>
+            <i className="fa-solid fa-user-group" />
+          </div>
+          <NavLink to="/groupchat">Group Chat</NavLink>
+        </div>
+
+        <div>
+          <div>
+            <i className="fa-solid fa-gear"></i>
+          </div>
+          <NavLink to="/settings">Settings</NavLink>
+        </div>
+      </div>
+
+      <div className="menu-logout">
+        <div>
+          <div>
+            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </div>
+          <NavLink to="/settings">Logout</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dropdown;
